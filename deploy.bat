ea6253b37bb1e9a507ac6ed942f7fd938598256a
sssthreemi-jpg <sssthreemi@gmail.com>
2026-04-04 21:38:42 +0900

    update

--- deploy.bat
+++ deploy.bat
@@ -1,10 +1,8 @@
 @echo off
 cd /d %~dp0
 git add -A
-set /p msg=커밋 메시지 입력 (엔터시 "update"):
-if "%msg%"=="" set msg=update
-git commit -m "%msg%"
+git commit -m "update"
 git push
 echo.
-echo 완료! 아무 키나 누르면 창이 닫힙니다.
-pause > nul
+echo Push complete!
+pause
