46638df98bcc8c618fe39d00f2134233b740d57b
sssthreemi-jpg <sssthreemi@gmail.com>
2026-04-05 22:17:00 +0900

    auto-deploy

--- server.js
+++ server.js
@@ -1569,11 +1569,11 @@ const server = http.createServer(async (req, res) => {
       }
       if (!stockCode) throw new Error('종목코드를 찾을 수 없습니다');
 
-      // 병렬로 기본정보 + integration + 일봉차트 요청
+      // 병렬로 기본정보 + integration + 일봉차트(fchart XML) 요청
       const [basicRes, integRes, dailyRes] = await Promise.allSettled([
         proxyRequest(`${mobileBase}/stock/${stockCode}/basic`),
         proxyRequest(`${mobileBase}/stock/${stockCode}/integration`),
-        proxyRequest(`${mobileBase}/stock/${stockCode}/price?count=150`),
+        proxyRequest(`https://fchart.stock.naver.com/sise.nhn?symbol=${stockCode}&timeframe=day&count=150&requestType=0`),
       ]);
 
       let basic = null, integ = null;
